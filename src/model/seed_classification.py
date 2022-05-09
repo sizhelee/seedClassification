20c090df0b8797218d563d6113d6a6b1e1da48c2
Sizhe <lisizhe@pku.edu.cn>
2022-05-09 21:36:10 +0800

    HOG+SVM

--- src/model/seed_classification.py
+++ src/model/seed_classification.py
@@ -1,10 +1,39 @@
-from utils import io_util
+import os
+import sys
+sys.path.append(os.path.abspath("."))
 
+from src.utils import io_util
+from skimage.feature import hog
+from sklearn import svm
 
-def main():
+import numpy as np
 
-    config = io_util.load_yaml("./config.yml", True)
+def main(config):
+    train_x, train_y, _ = io_util.load_img(config["model"]["dataloader"])
+    test_x, _, test_img = io_util.load_img(config["model"]["dataloader"], mode="test")
+    
+    train_X = []
+    for img in train_x:
+        fd = hog(img, pixels_per_cell=(4, 4), cells_per_block=(2, 2), multichannel=True, feature_vector=True)
+        train_X.append(fd)
+    train_X = np.array(train_X)
+
+    clf = svm.SVC(C=0.01, kernel="poly")
+    clf.fit(train_X, train_y)
+    print("Training Score: {}".format(clf.score(train_X, train_y)))
+    print("Finished Training!")
+
+    test_X = []
+    for img in test_x:
+        fd = hog(img, pixels_per_cell=(4, 4), cells_per_block=(2, 2), multichannel=True, feature_vector=True)
+        test_X.append(fd)
+    test_X = np.array(test_X)
+
+    test_Y = clf.predict(test_X)
+    io_util.generate_csv(test_Y, test_img, config["model"]["results"])
+
 
 
 if __name__ == "__main__":
-    main()
+    config = io_util.load_yaml("src/experiments/config.yml", True)
+    main(config)
--- src/utils/io_util.py
+++ src/utils/io_util.py
@@ -1,4 +1,29 @@
 import yaml
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm, trange
+import pandas as pd
+from pandas import DataFrame
+
+class2id = {
+    "Black-grass": 0,
+    "Charlock": 1,
+    "Cleavers": 2,
+    "Common Chickweed": 3,
+    "Common wheat": 4,
+    "Fat Hen": 5,
+    "Loose Silky-bent": 6,
+    "Maize": 7,
+    "Scentless Mayweed": 8,
+    "Shepherds Purse": 9,
+    "Small-flowered Cranesbill": 10,
+    "Sugar beet": 11
+}
+
+id2class = ["Black-grass", "Charlock", "Cleavers", "Common Chickweed",
+ "Common wheat", "Fat Hen", "Loose Silky-bent", "Maize", "Scentless Mayweed", 
+ "Shepherds Purse", "Small-flowered Cranesbill", "Sugar beet"]
 
 
 def load_yaml(file_path, verbose=True):
@@ -7,3 +32,55 @@ def load_yaml(file_path, verbose=True):
     if verbose:
         print("Load yaml file from {}".format(file_path))
     return yml_file
+
+
+def load_img(cfg, mode="train"):
+
+    X = []
+    Y = []
+    img_name = []
+
+    folder_path = cfg["{}_path".format(mode)]
+    img_size = (cfg["img_width"], cfg["img_height"])
+
+    if mode == "train":
+        folders = os.listdir(folder_path)
+        with tqdm(total=4750) as pbar:
+            pbar.set_description('Loading {} data from {}'.format(mode, folder_path))
+            for folder in folders:
+                imgs = os.listdir("{}/{}".format(folder_path, folder))
+                for img in imgs:
+                    img_path = "{}/{}/{}".format(folder_path, folder, img)
+                    I = cv2.imread(img_path)
+                    I = cv2.resize(I, img_size, interpolation=cv2.INTER_CUBIC)
+                    X.append(I)
+                    Y.append(class2id[folder])
+                    img_name.append(img)
+                    pbar.update(1)
+
+    else:
+        imgs = os.listdir(folder_path)
+        with tqdm(total=794) as pbar:
+            pbar.set_description('Loading {} data from {}'.format(mode, folder_path))
+            for img in imgs:
+                img_path = "{}/{}".format(folder_path, img)
+                I = cv2.imread(img_path)
+                I = cv2.resize(I, img_size, interpolation=cv2.INTER_CUBIC)
+                X.append(I)
+                img_name.append(img)
+                pbar.update(1)
+
+    X = np.stack(X)
+    Y = np.array(Y)
+    print("data size: {}, label size: {}".format(X.shape, Y.shape))
+    return X, Y, img_name
+
+
+def generate_csv(predict, img_name, cfg):
+    data = {
+        "file": img_name, 
+        "species": [id2class[i] for i in predict]
+    }
+    df = DataFrame(data)
+    df.to_csv(cfg["result_path"], index=False)
+    
